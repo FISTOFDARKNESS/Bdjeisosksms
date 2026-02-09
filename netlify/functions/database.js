593fbffae8cc4a1b2eaebfabe9b07211dcb17399
_.yzero <116497775+FISTOFDARKNESS@users.noreply.github.com>
2026-02-09 01:42:55 +0100

    Update database.js

--- netlify/functions/database.js
+++ netlify/functions/database.js
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
 export const handler = async (event) => {
-  const sql = neon(process.env.DATABASE_URL);
+  const sql = neon("postgresql://neondb_owner:npg_ZIigvq76hfeD@ep-lucky-night-ahauzl87-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
 
   try {
     if (event.httpMethod === 'GET') {
